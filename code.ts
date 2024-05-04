b7e326388f0115342cc2ea3b8e9a9ce575e04504
Matsuhisa Hironobu <matsuhisa+github@gmail.com>
2024-05-05 01:53:51 +0900

    findAll で取得した node を変換する

--- code.ts
+++ code.ts
@@ -3,9 +3,32 @@ figma.showUI(__html__);
 figma.ui.onmessage =  (msg: {type: string, file_key: string}) => {
 
   if (msg.type === 'create-catalog') {
-    const file_key = msg.file_key;
-    console.log(file_key);
+    // const file_key = msg.file_key;
+    const page = figma.currentPage;
+
+    const sectionNodes = page.findAll(node => node.type === 'SECTION');
+
+    // MEMO: findAll で取得すると SceneNode になる。SectionNode ではない。
+    // https://www.typescriptlang.org/docs/handbook/advanced-types.html
+    // https://forum.figma.com/t/type-scenenode-is-not-assignable-to-type-componentnode/19694/2
+    sectionNodes.forEach((sectionNode) => {
+      const section = sectionNode as SectionNode;
+
+      section.children.forEach((child) => {
+        if(child.type === 'FRAME') {
+          const frame = child as FrameNode;
+          console.log(frame.name);
+        }
+      });
+
+    });
+
+    // const sectionNodes = page.findAll(node => node.type === 'SECTION');
+    // sectionNodes.forEach((sectionNode) => {
+    //   const sectionName = sectionNode.name;
+    //   console.log(sectionNode.parent);
+    // });
   }
 
-  figma.closePlugin();
+  // figma.closePlugin();
 };
