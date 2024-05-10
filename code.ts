3584247c16bb6760ef68d933e9cb65e493c80ac4
Matsuhisa Hironobu <matsuhisa+github@gmail.com>
2024-05-10 23:20:22 +0900

    WIP

--- code.ts
+++ code.ts
@@ -6,8 +6,19 @@ figma.ui.onmessage =  (msg: {type: string, file_key: string}) => {
     // const file_key = msg.file_key;
     const page = figma.currentPage;
 
-    const sectionNodes = page.findAll(node => node.type === 'SECTION');
+    // MEMO: 最終的にまとめる Frame を作る
+    const wrapFrame = figma.createFrame();
+    wrapFrame.layoutMode = "HORIZONTAL";
+    wrapFrame.itemSpacing = 40;
+    wrapFrame.horizontalPadding = 0;
+    wrapFrame.verticalPadding = 0;
+    wrapFrame.layoutSizingHorizontal = "HUG";
+    wrapFrame.layoutSizingVertical = "HUG";
+    wrapFrame.fills = [{ type: "SOLID", color: { r: 255/255, g: 255/255, b: 255/255 }, opacity: 0 }];
 
+    console.log(page);
+
+    const sectionNodes = page.findAll(node => node.type === 'SECTION');
     sectionNodes.forEach((sectionNode) => {
       // MEMO: findAll で取得すると SceneNode になる。SectionNode ではない。
       // https://www.typescriptlang.org/docs/handbook/advanced-types.html
@@ -18,23 +29,25 @@ figma.ui.onmessage =  (msg: {type: string, file_key: string}) => {
       const frame = figma.createFrame();
       frame.layoutMode = "VERTICAL";
       frame.itemSpacing = 20;
-      frame.horizontalPadding = 20;
-      frame.verticalPadding = 20;
+      frame.horizontalPadding = 40;
+      frame.verticalPadding = 40;
       frame.layoutSizingHorizontal = "HUG";
       frame.name = section.name;
 
-      const colorRound = 0.10196078568696976;
       frame.strokes = [{ type: "SOLID", color: { r: 184/255, g: 28/255, b: 37/255 }}];
       frame.strokeWeight = 10;
       frame.dashPattern = [20, 20];
 
       // MEMO: テキスト情報を作る => Frame に追加する
       (async () => {
+        // Regular
         await figma.loadFontAsync({ family: "Inter", style: "Regular" });
+        // textLabel.fontName = { family: "Inter", style: "Regular" };
         const textLabel = figma.createText();
         textLabel.characters = section.name;
         textLabel.fontSize = 40;
-
+        textLabel.fontName = { family: "Inter", style: "Regular" };
+  
         // frame.appendChild(textLabel);
         frame.insertChild(0, textLabel);
       })();
@@ -42,7 +55,6 @@ figma.ui.onmessage =  (msg: {type: string, file_key: string}) => {
       section.children.forEach((child, index) => {
         if(child.type === 'FRAME') {
           const childFrame = child as FrameNode;
-          console.log(childFrame.name);
 
           (async () => {
             const bytes = await childFrame.exportAsync({
@@ -67,7 +79,7 @@ figma.ui.onmessage =  (msg: {type: string, file_key: string}) => {
         }
       });
 
-
+      wrapFrame.appendChild(frame);
     });
 
     // const sectionNodes = page.findAll(node => node.type === 'SECTION');
